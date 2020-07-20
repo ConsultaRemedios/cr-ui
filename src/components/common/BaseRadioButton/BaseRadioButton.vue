5e0e876d3b249ff6304d535d840d211abb21c083
João Pedrozo <jp.pedrozo@hotmail.com>
2020-07-20 15:50:15 -0300

    Linting arrumado

--- src/components/common/BaseRadioButton/BaseRadioButton.vue
+++ src/components/common/BaseRadioButton/BaseRadioButton.vue
@@ -16,118 +16,118 @@
 </template>
 
 <script>
-export default {
-  inheritAttrs: false,
-  name: 'BaseRadioButton',
+  export default {
+    inheritAttrs: false,
+    name: 'BaseRadioButton',
 
-  props: {
-    label: {
-      type: String,
-      default: '',
-    },
-    name: {
-      type: String,
-      required: true,
+    props: {
+      label: {
+        type: String,
+        default: '',
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+
+      value: {
+        type: String,
+        required: true,
+      },
+
+      checked: {
+        type: Boolean,
+        default: false,
+      },
+
+      model: {
+        type: String,
+        default: '',
+      },
     },
 
-    value: {
-      type: String,
-      required: true,
+    methods: {
+      onChange() {
+        this.$emit('change', {
+          name: this.model || this.name,
+          value: this.value,
+        });
+      },
     },
-
-    checked: {
-      type: Boolean,
-      default: false,
-    },
-
-    model: {
-      type: String,
-      default: '',
-    },
-  },
-
-  methods: {
-    onChange() {
-      this.$emit('change', {
-        name: this.model || this.name,
-        value: this.value,
-      });
-    },
-  },
-};
+  };
 </script>
 
 <style module>
-.wrapper {
-  cursor: pointer;
-  display: block;
-  position: relative;
-  user-select: none;
-  cursor: pointer;
-  min-height: 25px;
-}
+  .wrapper {
+    cursor: pointer;
+    display: block;
+    position: relative;
+    user-select: none;
+    cursor: pointer;
+    min-height: 25px;
+  }
 
-.wrapper::before {
-  content: "";
-  display: inline-block;
-  width: 25px;
-  height: 25px;
-  margin-right: 5px;
-  vertical-align: top;
-  font-weight: 400;
-}
+  .wrapper::before {
+    content: "";
+    display: inline-block;
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+    vertical-align: top;
+    font-weight: 400;
+  }
 
-.wrapper:hover .checkmark {
-  border-color: #138484;
-}
+  .wrapper:hover .checkmark {
+    border-color: #138484;
+  }
 
-.label {
-  display: inline-block;
-  vertical-align: top;
-  padding-top: 3px;
-  font-weight: 400;
-  line-height: 1.4;
-}
+  .label {
+    display: inline-block;
+    vertical-align: top;
+    padding-top: 3px;
+    font-weight: 400;
+    line-height: 1.4;
+  }
 
-.checkmark {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 24px;
-  width: 24px;
-  background-color: white;
-  border-radius: 50%;
-  border: 1px solid #595959;
-}
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 24px;
+    width: 24px;
+    background-color: white;
+    border-radius: 50%;
+    border: 1px solid #595959;
+  }
 
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
 
-.wrapper input {
-  position: absolute;
-  opacity: 0;
-}
+  .wrapper input {
+    position: absolute;
+    opacity: 0;
+  }
 
-.wrapperSelected .checkmark {
-  display: block;
-  border-color: #138484;
-  background-color: #138484;
+  .wrapperSelected .checkmark {
+    display: block;
+    border-color: #138484;
+    background-color: #138484;
 
-}
+  }
 
-.wrapperSelected .checkmark:after {
-  display: block;
-  left: 7px;
-  width: 7px;
-  height: 10px;
-  border: solid #fff;
-  margin-top: 5.3px;
-  border-radius: 1px;
-  border-width: 0 3px 3px 0;
-  transform: rotate(45deg);
-  background: #138484;
-}
+  .wrapperSelected .checkmark:after {
+    display: block;
+    left: 7px;
+    width: 7px;
+    height: 10px;
+    border: solid #fff;
+    margin-top: 5.3px;
+    border-radius: 1px;
+    border-width: 0 3px 3px 0;
+    transform: rotate(45deg);
+    background: #138484;
+  }
 </style>
